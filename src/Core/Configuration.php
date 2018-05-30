5571dbcdf2d6ed99dcdd09947120e5a7fa82a333
alain tiemblo <ninsuo@gmail.com>
2018-05-30 06:49:39 +0200

    add color tags

--- src/Core/Configuration.php
+++ src/Core/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Lazy\Core;
 
+use Webmozart\Console\Api\Formatter\Style;
 use Webmozart\Console\Config\DefaultApplicationConfig;
 
 class Configuration extends DefaultApplicationConfig
@@ -10,8 +11,16 @@ class Configuration extends DefaultApplicationConfig
     {
         parent::configure();
 
-        return $this
+        $this
             ->setName('lazy')
             ->setVersion('0.0.1');
+
+        // Configure color tags (to use <red>bob</red> for example)
+        $colors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'];
+        foreach ($colors as $color) {
+            $this->addStyle(Style::tag($color)->fg($color));
+        }
+
+        return $this;
     }
 }
